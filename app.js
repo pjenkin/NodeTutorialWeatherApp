9339305eaf533004867bc38174004a6e9cda3b4c
Peter N Jenkin <peternjenkin@gmail.com>
2019-02-04 19:18:26 +0000

    MqpQuest lat lng tryout

---
+++ app.js
@@ -0,0 +1,16 @@
+const request = require('request');
+
+request({
+  url: 'http://www.mapquestapi.com/geocoding/v1/address?key=pX8PCBe1xKpGx9ZBf05T9bXmJU1kePLv&location=67%20Trevingey%20Road%20Redruth',
+  json: true
+}, (error, response, body) => {
+  console.log('Printing body: ',body);
+  console.log('Printing lat: ',body.results[0].locations[0].latLng.lat);
+  console.log('Printing lng: ',body.results[0].locations[0].latLng.lng);  
+});
+
+
+// https://www.udemy.com/the-complete-nodejs-developer-course-2/learn/v4/questions/2956062
+// pX8PCBe1xKpGx9ZBf05T9bXmJU1kePLv
+// http://www.mapquestapi.com/geocoding/v1/address?key=KEY&location=1301%20lombard%20street%20philadelphi
+// http://www.mapquestapi.com/geocoding/v1/address?key=pX8PCBe1xKpGx9ZBf05T9bXmJU1kePLv&location=67%20Trevingey%20Road%20Redruth
