994eb73406f9b3d3cccc3f4ed5d367687395f479
Peter N Jenkin <peternjenkin@gmail.com>
2019-02-04 14:05:54 +0000

    4-26 setTimeout & callback delays

--- playground/async-basics.js
+++ playground/async-basics.js
@@ -4,4 +4,9 @@ setTimeout(() => {
   console.log('Inside of  callback')
 },2000);
 
+setTimeout(() =>
+{
+  console.log('This callback\'s delay should be zero milliseconds')
+},0);   // even with zero millisecond delay, printed after final non-async log
+
 console.log('Finishing up');
