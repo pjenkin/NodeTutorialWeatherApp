2ddf59f9b4e1820f0011ac53da23bd13b864345d
Peter N Jenkin <peternjenkin@gmail.com>
2019-02-06 10:32:03 +0000

    basic forecast.io/darksky.net API request for temperature

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*
 // const request = require('request');
 const yargs = require('yargs');
 // const _ = require('lodash');
@@ -21,12 +22,12 @@ const argv = yargs
 
 debugger;
 
-/*
-if (geocode.geocodeAddress(argv.address) > 0)
-{
-  console.log('Error with geocoding');
-}
-*/
+
+// if (geocode.geocodeAddress(argv.address) > 0)
+// {
+//   console.log('Error with geocoding');
+// }
+
 
 geocode.geocodeAddress(argv.address, (errorMessage, results) => {
   if (errorMessage) {
@@ -36,4 +37,20 @@ geocode.geocodeAddress(argv.address, (errorMessage, results) => {
         console.log(JSON.stringify(results, undefined, 2))
     }
   });
-// }
+*/
+
+// import request
+// make request
+// print current temperature
+
+var request = require('request');
+var _ = require('lodash');
+
+request({
+  url: 'https://api.darksky.net/forecast/a46666c475d10436fc8f2b04f56bb46c/50.22,-5.23',
+  json: true
+}, (error, response, body) => {
+  console.log('location temperature : ', _.get(body,'currently.temperature'));
+});
+
+// using my own darksky developer API key a46666c475d10436fc8f2b04f56bb46c
