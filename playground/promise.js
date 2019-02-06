2dc39f847d6cdbec394f4e28909b0ff1c429ec8a
Peter N Jenkin <peternjenkin@gmail.com>
2019-02-06 15:15:41 +0000

    promise with resolve and reject and catch

--- playground/promise.js
+++ playground/promise.js
@@ -1,3 +1,20 @@
+var asyncAdd = (a,b) =>
+{
+  return new Promise((resolve, reject) =>
+  {
+    setTimeout(() =>
+    {
+      if (typeof a === 'number' && typeof b === 'number')
+      {
+        resolve(a+b);
+      } else
+      {
+        reject('Both arguments must be numbers');
+      } 
+    }, 3500);
+  });
+};
+
 var somePromise = new Promise((resolve, reject) => {
 setTimeout(() =>
 {
